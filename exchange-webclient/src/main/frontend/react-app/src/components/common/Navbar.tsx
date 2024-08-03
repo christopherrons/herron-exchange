8f69c2e95a64e22c7f3ad6e2acb024edce1a4567
christopherrons <christopherheron09@gmail.com>
2024-08-03 19:32:59 +0200

    Add footer

---
+++ exchange-webclient/src/main/frontend/react-app/src/components/common/Navbar.tsx
@@ -0,0 +1,20 @@
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+            alt="Logo"
+            width="30"
+            height="24"
+            className="d-inline-block align-text-top"
+          />
+          Bootstrap
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
